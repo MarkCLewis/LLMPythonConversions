03f657cf942d9a2ffd533a1ba78c8dfab0f2d709
Mark Lewis <mlewis@lmde6>
2025-10-12 12:32:49 -0500

    Small benchmark timing completed

--- generate-inputs.sh
+++ generate-inputs.sh
@@ -2,5 +2,5 @@ cd fasta/Version3/Human/
 make c
 ./a.out 25000000 > ../../../k-nuclcotide/knucleotide-input25000000.txt
 ./a.out 25000000 > ../../../regex-redux/regexredux-input5000000.txt
-./a.out 100000001 > ../../../regex-redux/revcomp-input.txt
+./a.out 1000000001 > ../../../reverse-complement/revcomp-input.txt
 
--- processTimes.scala
+++ processTimes.scala
@@ -1,6 +1,17 @@
 import java.io.File
+import scala.collection.mutable
 
 val lineRegex = """([A-Za-z0-9\.\-]+) ([A-Za-z0-9\._]+) ([A-Za-z0-9\._]+) ([A-Za-z0-9\._]+)|(real|user)\s+(\d+)m(\d+.\d+)s""".r
+
+val benchmarks = List("k-nuclcotide", "spectral-norm",
+  "binary-trees",   "mandelbrot",
+  "fannkuch-redux", "nbody",        "regex-redux",
+  "fasta",          "pidigits",     "reverse-complement"
+).sorted
+
+val authors = List("Human", "Gemini2.5", "ChatGPT", "Claude")
+
+val languages = List("python", "c", "rust")
 
 def stats(times: List[Double]): (Double, Double) = {
   val avg = times.sum / times.length
@@ -8,7 +19,7 @@ def stats(times: List[Double]): (Double, Double) = {
   (avg, std)
 }
 
-def processTimeFile(file: File): Unit = {
+def processTimeFile(file: File): Map[(String, String, String, String), String] = {
   var benchmark = ""
   var version = ""
   var author = ""
@@ -17,12 +28,15 @@ def processTimeFile(file: File): Unit = {
   var userSecs: List[Double] = Nil
   val source = io.Source.fromFile(file)
   val lines = source.getLines()
+  val values = mutable.Buffer[((String, String, String, String), String)]()
   for (case lineRegex(bench, ver, auth, lang, ru, m, s) <- lines) {
     if (bench != null) {
       if (realSecs.nonEmpty) {
         val (ravg, rstd) = stats(realSecs)
         val (uavg, ustd) = stats(userSecs)
-        println(f"$benchmark $version $author $language \\shortstack[c]{$$$ravg%1.2f \\pm $rstd%1.2f$$ \\\\ $$${uavg/ravg}%1.2f$$}")
+        val tableEntry = f"$$$ravg%1.2f \\pm $rstd%1.2f$$"
+        println(f"$benchmark $version $author $language $tableEntry")
+        values += (((benchmark, language, version, author), tableEntry))
         realSecs = Nil
         userSecs = Nil
       }
@@ -40,10 +54,28 @@ def processTimeFile(file: File): Unit = {
   }
   val (ravg, rstd) = stats(realSecs)
   val (uavg, ustd) = stats(userSecs)
-  println(f"$benchmark $version $author $language \\shortstack[c]{$$$ravg%1.2f \\pm $rstd%1.2f$$ \\\\ $$${uavg/ravg}%1.2f$$}")
+  val tableEntry = f"$$$ravg%1.2f \\pm $rstd%1.2f$$"
+  println(f"$benchmark $version $author $language $tableEntry")
+  values += (((benchmark, language, version, author), tableEntry))
   source.close()
+  values.groupBy(_(0)).map { case (key, lst) =>
+    key -> lst.head(1)
+  }
 }
 
-@main def processTimes(timeFileName: String): Unit = {
-  processTimeFile(new File(timeFileName))
+@main def processTimes(timeFileNames: String*): Unit = {
+  val data = timeFileNames.foldLeft(Map[(String, String, String, String), String]())((m, timeFileName) => m ++ processTimeFile(new File(timeFileName)))
+  println(data)
+  for bench <- benchmarks do {
+    for i <- 1 to 8 do {
+      val version = f"Version$i"
+      for language <- languages do {
+        val values = authors.map { case author =>
+          val key = (bench, language, version, author)
+          data.get(key).getOrElse("")
+        }
+        if values.exists(_.nonEmpty) then println(f"$bench, $language, $version, ${values.mkString(" & ")}")
+      }
+    }
+  }
 }
